4de4568859a3127979606cb30de0c5b757bacbd1
cadumeneses <carloseduardoms646@gmail.com>
2022-11-07 09:26:51 -0300

    test: ensure remoteAuthentication calls httpclient with correct url

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -3,6 +3,6 @@ import '../entities/account_entity.dart';
 abstract class Authentication {
   Future<AccountEntity> auth(
       {required String email, required String password}) {
-
+        return auth(email: email, password: password);
       }
 }
