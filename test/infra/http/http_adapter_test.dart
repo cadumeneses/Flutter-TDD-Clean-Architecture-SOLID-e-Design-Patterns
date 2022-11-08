5585d8cdbba2c6dde120165303e29d3529ca5531
cadumeneses <carloseduardoms646@gmail.com>
2022-11-08 10:00:21 -0300

    test: ensure httpadapter calls post withdout body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -9,26 +9,31 @@ import 'package:mocktail/mocktail.dart';
 class HttpAdapter {
   final Client client;
   HttpAdapter({required this.client});
-  Future<dynamic> request({ required String url, required String method, Map? body, Map? headers }) async {
-    final defaultHeaders = headers?.cast<String, String>() ?? {}..addAll({
-      'content-type': 'application/json',
-      'accept': 'application/json'
-    });
+  Future<dynamic> request(
+      {required String url,
+      required String method,
+      Map? body,
+      Map? headers}) async {
+    final defaultHeaders = headers?.cast<String, String>() ?? {}
+      ..addAll(
+          {'content-type': 'application/json', 'accept': 'application/json'});
     final jsonBody = body != null ? jsonEncode(body) : null;
     var response = Response('', 500);
     Future<Response>? futureResponse;
     try {
       if (method == 'post') {
-        futureResponse = client.post(Uri.parse(url), headers: defaultHeaders, body: jsonBody);
+        futureResponse = client.post(Uri.parse(url),
+            headers: defaultHeaders, body: jsonBody);
       } else if (method == 'get') {
         futureResponse = client.get(Uri.parse(url), headers: defaultHeaders);
       } else if (method == 'put') {
-        futureResponse = client.put(Uri.parse(url), headers: defaultHeaders, body: jsonBody);
+        futureResponse =
+            client.put(Uri.parse(url), headers: defaultHeaders, body: jsonBody);
       }
       if (futureResponse != null) {
         response = await futureResponse.timeout(const Duration(seconds: 10));
       }
-    } catch(error) {
+    } catch (error) {
       throw HttpError.serverError;
     }
     return _handleResponse(response);
@@ -36,12 +41,18 @@ class HttpAdapter {
 
   dynamic _handleResponse(Response response) {
     switch (response.statusCode) {
-      case 200: return response.body.isEmpty ? null : jsonDecode(response.body);
-      case 204: return null;
-      case 400: throw HttpError.badRequest;
-      case 401: throw HttpError.unauthorized;
-      case 404: throw HttpError.notFound;
-      default: throw HttpError.serverError;
+      case 200:
+        return response.body.isEmpty ? null : jsonDecode(response.body);
+      case 204:
+        return null;
+      case 400:
+        throw HttpError.badRequest;
+      case 401:
+        throw HttpError.unauthorized;
+      case 404:
+        throw HttpError.notFound;
+      default:
+        throw HttpError.serverError;
     }
   }
 }
@@ -53,21 +64,26 @@ class ClientSpy extends Mock implements Client {
     mockGet(200);
   }
 
-  When mockPostCall() => when(() => this.post(any(), body: any(named: 'body'), headers: any(named: 'headers')));
-  void mockPost(int statusCode, {String body = '{"any_key":"any_value"}'}) => mockPostCall().thenAnswer((_) async => Response(body, statusCode));
+  When mockPostCall() => when(() => this
+      .post(any(), body: any(named: 'body'), headers: any(named: 'headers')));
+  void mockPost(int statusCode, {String body = '{"any_key":"any_value"}'}) =>
+      mockPostCall().thenAnswer((_) async => Response(body, statusCode));
   void mockPostError() => when(() => mockPostCall().thenThrow(Exception()));
 
-  When mockPutCall() => when(() => this.put(any(), body: any(named: 'body'), headers: any(named: 'headers')));
-  void mockPut(int statusCode, {String body = '{"any_key":"any_value"}'}) => mockPutCall().thenAnswer((_) async => Response(body, statusCode));
+  When mockPutCall() => when(() => this
+      .put(any(), body: any(named: 'body'), headers: any(named: 'headers')));
+  void mockPut(int statusCode, {String body = '{"any_key":"any_value"}'}) =>
+      mockPutCall().thenAnswer((_) async => Response(body, statusCode));
   void mockPutError() => when(() => mockPutCall().thenThrow(Exception()));
 
-  When mockGetCall() => when(() => this.get(any(), headers: any(named: 'headers')));
-  void mockGet(int statusCode, {String body = '{"any_key":"any_value"}'}) => mockGetCall().thenAnswer((_) async => Response(body, statusCode));
+  When mockGetCall() =>
+      when(() => this.get(any(), headers: any(named: 'headers')));
+  void mockGet(int statusCode, {String body = '{"any_key":"any_value"}'}) =>
+      mockGetCall().thenAnswer((_) async => Response(body, statusCode));
   void mockGetError() => when(() => mockGetCall().thenThrow(Exception()));
 }
 
 void main() {
-
   late HttpAdapter sut;
   late ClientSpy client;
   late String url;
@@ -83,26 +99,34 @@ void main() {
   });
   group('post', () {
     test('Should call post with correct values', () async {
-      await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
-      verify(() => client.post(
-        Uri.parse(url),
-        headers: {
-          'content-type': 'application/json',
-          'accept': 'application/json'
-        },
-        body: '{"any_key":"any_value"}'
-      ));
+      await sut
+          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
+      verify(() => client.post(Uri.parse(url),
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+          },
+          body: '{"any_key":"any_value"}'));
 
-      await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'}, headers: {'any_header': 'any_value'});
-      verify(() => client.post(
-        Uri.parse(url),
-        headers: {
-          'content-type': 'application/json',
-          'accept': 'application/json',
-          'any_header': 'any_value'
-        },
-        body: '{"any_key":"any_value"}'
-      ));
+      await sut.request(
+          url: url,
+          method: 'post',
+          body: {'any_key': 'any_value'},
+          headers: {'any_header': 'any_value'});
+      verify(() => client.post(Uri.parse(url),
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+            'any_header': 'any_value'
+          },
+          body: '{"any_key":"any_value"}'));
     });
+  });
+  test('Should call post withdoout body', () async {
+    await sut.request(url: url, method: 'post');
+    verify(() => client.post(
+          any(),
+          headers: any(named: 'headers'),
+        ));
   });
 }
