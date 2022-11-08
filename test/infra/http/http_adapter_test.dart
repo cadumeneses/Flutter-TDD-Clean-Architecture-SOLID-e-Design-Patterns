d46a3a4925e1611a88c973eecb37d7c728b817d3
cadumeneses <carloseduardoms646@gmail.com>
2022-11-08 10:43:04 -0300

    refactor: move httpadapter to its own file

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,61 +1,8 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
-import 'package:flutter_tdd/data/http/http_error.dart';
+import 'package:flutter_tdd/infra/http/http.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:mocktail/mocktail.dart';
-
-class HttpAdapter{
-  final Client client;
-  HttpAdapter({required this.client});
-  Future<dynamic> request(
-      {required String url,
-      required String method,
-      Map? body,
-      Map? headers}) async {
-    final defaultHeaders = headers?.cast<String, String>() ?? {}
-      ..addAll(
-          {'content-type': 'application/json', 'accept': 'application/json'});
-    final jsonBody = body != null ? jsonEncode(body) : null;
-    var response = Response('', 500);
-    Future<Response>? futureResponse;
-    try {
-      if (method == 'post') {
-        futureResponse = client.post(Uri.parse(url),
-            headers: defaultHeaders, body: jsonBody);
-      } else if (method == 'get') {
-        futureResponse = client.get(Uri.parse(url), headers: defaultHeaders);
-      } else if (method == 'put') {
-        futureResponse =
-            client.put(Uri.parse(url), headers: defaultHeaders, body: jsonBody);
-      }
-      if (futureResponse != null) {
-        response = await futureResponse.timeout(const Duration(seconds: 10));
-      }
-    } catch (error) {
-      throw HttpError.serverError;
-    }
-    return _handleResponse(response);
-  }
-
-  dynamic _handleResponse(Response response) {
-    switch (response.statusCode) {
-      case 200:
-        return response.body.isEmpty ? null : jsonDecode(response.body);
-      case 204:
-        return null;
-      case 400:
-        throw HttpError.badRequest;
-      case 401:
-        throw HttpError.unauthorized;
-      case 404:
-        throw HttpError.notFound;
-      default:
-        throw HttpError.serverError;
-    }
-  }
-}
 
 class ClientSpy extends Mock implements Client {
   ClientSpy() {
