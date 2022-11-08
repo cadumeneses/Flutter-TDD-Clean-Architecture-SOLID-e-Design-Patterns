81335dc212b1519d87bd3ab1e3e4e621f441c2c6
cadumeneses <carloseduardoms646@gmail.com>
2022-11-08 09:51:25 -0300

    test: ensure httpadapter calls post with correct headers

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,108 @@
+import 'dart:convert';
+
+import 'package:faker/faker.dart';
+import 'package:flutter_tdd/data/http/http_error.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:mocktail/mocktail.dart';
+
+class HttpAdapter {
+  final Client client;
+  HttpAdapter({required this.client});
+  Future<dynamic> request({ required String url, required String method, Map? body, Map? headers }) async {
+    final defaultHeaders = headers?.cast<String, String>() ?? {}..addAll({
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    });
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    var response = Response('', 500);
+    Future<Response>? futureResponse;
+    try {
+      if (method == 'post') {
+        futureResponse = client.post(Uri.parse(url), headers: defaultHeaders, body: jsonBody);
+      } else if (method == 'get') {
+        futureResponse = client.get(Uri.parse(url), headers: defaultHeaders);
+      } else if (method == 'put') {
+        futureResponse = client.put(Uri.parse(url), headers: defaultHeaders, body: jsonBody);
+      }
+      if (futureResponse != null) {
+        response = await futureResponse.timeout(const Duration(seconds: 10));
+      }
+    } catch(error) {
+      throw HttpError.serverError;
+    }
+    return _handleResponse(response);
+  }
+
+  dynamic _handleResponse(Response response) {
+    switch (response.statusCode) {
+      case 200: return response.body.isEmpty ? null : jsonDecode(response.body);
+      case 204: return null;
+      case 400: throw HttpError.badRequest;
+      case 401: throw HttpError.unauthorized;
+      case 404: throw HttpError.notFound;
+      default: throw HttpError.serverError;
+    }
+  }
+}
+
+class ClientSpy extends Mock implements Client {
+  ClientSpy() {
+    mockPost(200);
+    mockPut(200);
+    mockGet(200);
+  }
+
+  When mockPostCall() => when(() => this.post(any(), body: any(named: 'body'), headers: any(named: 'headers')));
+  void mockPost(int statusCode, {String body = '{"any_key":"any_value"}'}) => mockPostCall().thenAnswer((_) async => Response(body, statusCode));
+  void mockPostError() => when(() => mockPostCall().thenThrow(Exception()));
+
+  When mockPutCall() => when(() => this.put(any(), body: any(named: 'body'), headers: any(named: 'headers')));
+  void mockPut(int statusCode, {String body = '{"any_key":"any_value"}'}) => mockPutCall().thenAnswer((_) async => Response(body, statusCode));
+  void mockPutError() => when(() => mockPutCall().thenThrow(Exception()));
+
+  When mockGetCall() => when(() => this.get(any(), headers: any(named: 'headers')));
+  void mockGet(int statusCode, {String body = '{"any_key":"any_value"}'}) => mockGetCall().thenAnswer((_) async => Response(body, statusCode));
+  void mockGetError() => when(() => mockGetCall().thenThrow(Exception()));
+}
+
+void main() {
+
+  late HttpAdapter sut;
+  late ClientSpy client;
+  late String url;
+
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client: client);
+  });
+
+  setUpAll(() {
+    url = faker.internet.httpUrl();
+    registerFallbackValue(Uri.parse(url));
+  });
+  group('post', () {
+    test('Should call post with correct values', () async {
+      await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
+      verify(() => client.post(
+        Uri.parse(url),
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json'
+        },
+        body: '{"any_key":"any_value"}'
+      ));
+
+      await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'}, headers: {'any_header': 'any_value'});
+      verify(() => client.post(
+        Uri.parse(url),
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+          'any_header': 'any_value'
+        },
+        body: '{"any_key":"any_value"}'
+      ));
+    });
+  });
+}
