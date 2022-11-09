a208545b3f6ea9f1c994c4ad81bbf5d2e2f854b2
cadumeneses <carloseduardoms646@gmail.com>
2022-11-09 11:09:16 -0300

    refactor: move page initialization to a helper method

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -3,10 +3,14 @@ import 'package:flutter_tdd/ui/pages/pages.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets("Should load with correct initial state",
-      (WidgetTester tester) async {
+  Future<void> loadPage(WidgetTester tester) async {
     const loginPage = MaterialApp(home: LoginPage());
     await tester.pumpWidget(loginPage);
+  }
+
+  testWidgets("Should load with correct initial state",
+      (WidgetTester tester) async {
+    await loadPage(tester);
 
     final emailTextChildren = find.descendant(
       of: find.bySemanticsLabel('Email'),
@@ -33,7 +37,12 @@ void main() {
     );
 
     final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
-    
+
     expect(button.onPressed, null);
+  });
+
+  testWidgets('Shold call validate with correct values',
+      (WidgetTester tester) async {
+    await loadPage(tester);
   });
 }
