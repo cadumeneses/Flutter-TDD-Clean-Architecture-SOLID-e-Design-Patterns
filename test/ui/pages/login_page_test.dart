23f9cc1638db0a21ce7f2cce37d992c4ef8cea5d
cadumeneses <carloseduardoms646@gmail.com>
2022-11-09 11:03:42 -0300

    test: ensure loginpage loads with corrects init state

---
+++ test/ui/pages/login_page_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tdd/ui/pages/pages.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets("Should load with correct initial state",
+      (WidgetTester tester) async {
+    const loginPage = MaterialApp(home: LoginPage());
+    await tester.pumpWidget(loginPage);
+
+    final emailTextChildren = find.descendant(
+      of: find.bySemanticsLabel('Email'),
+      matching: find.byType(Text),
+    );
+
+    expect(
+      emailTextChildren,
+      findsOneWidget,
+      reason:
+          'When a TextFormField has only one text child, means it has no errors, since one of the childs is always the labels texts',
+    );
+
+    final passwordTextChildren = find.descendant(
+      of: find.bySemanticsLabel('Senha'),
+      matching: find.byType(Text),
+    );
+
+    expect(
+      passwordTextChildren,
+      findsOneWidget,
+      reason:
+          'When a TextFormField has only one text child, means it has no errors, since one of the childs is always the labels texts',
+    );
+
+    final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
+    
+    expect(button.onPressed, null);
+  });
+}
