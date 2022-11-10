c981294983feadfa96521a9c96d7a90ff9055b51
cadumeneses <carloseduardoms646@gmail.com>
2022-11-10 11:38:41 -0300

    refactor: move sut creation to the setup method

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -18,10 +18,17 @@ class StreamLoginPresenter {
 }
 
 void main() {
+  late ValidationSpy validation;
+  late StreamLoginPresenter sut;
+  late String email;
+
+  setUp(() {
+    validation = ValidationSpy();
+    sut = StreamLoginPresenter(validation: validation);
+    email = faker.internet.email();
+  });
+  
   test('Should call Validation with correct email', () {
-    final validation = ValidationSpy();
-    final sut = StreamLoginPresenter(validation: validation);
-    final email = faker.internet.email();
     sut.validateEmail(email);
     verify(() => validation.validate(field: 'email', value: email)).called(1);
   });
