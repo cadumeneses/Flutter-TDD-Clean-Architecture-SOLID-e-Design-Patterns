97218b97f6545a629967a0a96e94f123c0e9af0f
cadumeneses <carloseduardoms646@gmail.com>
2022-11-17 10:16:32 -0300

    test: ensure ValidationComposite returns first error with correct field

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -11,7 +11,7 @@ class ValidationComposite implements Validation {
 
   String? validate({required String field, required String value}) {
     String? error;
-    for (final validation in validations) {
+    for (final validation in validations.where((e) => e.field == field)) {
       error = validation.validate(value);
       if (error?.isNotEmpty == true) {
         return error;
@@ -27,6 +27,7 @@ void main() {
   late ValidationComposite sut;
   late FieldValidationSpy validation1;
   late FieldValidationSpy validation2;
+  late FieldValidationSpy validation3;
 
   void mockValidation1(String? error) {
     when(() => validation1.validate(any())).thenReturn(error);
@@ -36,14 +37,21 @@ void main() {
     when(() => validation2.validate(any())).thenReturn(error);
   }
 
+  void mockValidation3(String? error) {
+    when(() => validation3.validate(any())).thenReturn(error);
+  }
+
   setUp(() {
     validation1 = FieldValidationSpy();
-    when(() => validation1.field).thenReturn('any_field');
+    when(() => validation1.field).thenReturn('other_field');
     mockValidation1(null);
     validation2 = FieldValidationSpy();
     when(() => validation2.field).thenReturn('any_field');
     mockValidation2(null);
-    sut = ValidationComposite([validation1, validation2]);
+    validation3 = FieldValidationSpy();
+    when(() => validation3.field).thenReturn('any_field');
+    mockValidation3(null);
+    sut = ValidationComposite([validation1, validation2, validation3]);
   });
 
   test('Should return null if all validations returns null or empty', () {
@@ -57,9 +65,10 @@ void main() {
   test('Should return the first error', () {
     mockValidation1('error_1');
     mockValidation2('error_2');
+    mockValidation3('error_3');
 
     final error = sut.validate(field: 'any_field', value: 'any_value');
 
-    expect(error, 'error_1');
+    expect(error, 'error_2');
   });
 }
