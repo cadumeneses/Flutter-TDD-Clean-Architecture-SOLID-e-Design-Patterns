ab7cfc973f296e3a73ba6ff60a1d685b90ba7088
cadumeneses <carloseduardoms646@gmail.com>
2022-11-23 10:47:49 -0300

    test: ensure SplashPage calls loadCurrentAccount on page load

--- test/ui/pages/splash_page_test.dart
+++ test/ui/pages/splash_page_test.dart
@@ -1,37 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
+import 'package:mocktail/mocktail.dart';
 
 class SplashPage extends StatelessWidget {
-  const SplashPage({super.key});
+  final SplashPresenter presenter;
+  const SplashPage({required this.presenter, super.key});
 
   @override
   Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('4Dev'),
       ),
-      body: const CircularProgressIndicator(),
+      body: const Center(child: CircularProgressIndicator()),
     );
   }
 }
 
+abstract class SplashPresenter {
+  Future<void> loadCurrentAccount();
+}
+
+class SplashPrensenterSpy extends Mock implements SplashPresenter {
+  SplashPrensenterSpy() {
+    when(() => loadCurrentAccount()).thenAnswer((_) async => _);
+  }
+}
+
 void main() {
+  late SplashPrensenterSpy presenter;
+
   Future<void> loadPage(WidgetTester tester) async {
+    presenter = SplashPrensenterSpy();
+
     await tester.pumpWidget(GetMaterialApp(
       initialRoute: '/',
       getPages: [
-        GetPage(
-          name: '/',
-          page: () => const SplashPage(),
-        ),
+        GetPage(name: '/', page: () => SplashPage(presenter: presenter))
       ],
     ));
   }
 
   testWidgets('Should presenter spinner on page load',
       (WidgetTester tester) async {
-        await loadPage(tester);
+    await loadPage(tester);
+
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('Should call loadCurrentAccount on page load',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    verify(() => presenter.loadCurrentAccount()).called(1);
   });
 }
