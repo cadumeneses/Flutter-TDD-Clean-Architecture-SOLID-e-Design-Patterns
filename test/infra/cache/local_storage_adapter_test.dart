a12157b7ea9b042a48f65b2c32ca634145fe2a15
cadumeneses <carloseduardoms646@gmail.com>
2022-11-23 09:26:14 -0300

    test: ensure LocalStorageAdapter calls fecthSecure with correct values

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -15,31 +15,45 @@ class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {
   void mockSaveError() =>
       when(() => write(key: any(named: 'key'), value: any(named: 'value')))
           .thenThrow(Exception());
+  
+  When mockFetchCall() => when(() => read(key: any(named: 'key')));
+  void mockFetch(String? key) => mockFetchCall().thenAnswer((_) async => key);
 }
 
 void main() {
   late FlutterSecureStorageSpy secureStorage;
   late LocalStorageAdapter sut;
-  late String key;
+  String? key;
   late String value;
 
   setUp(() {
     secureStorage = FlutterSecureStorageSpy();
+    secureStorage.mockFetch(key);
     sut = LocalStorageAdapter(secureStorage: secureStorage);
     key = faker.lorem.word();
     value = faker.guid.guid();
   });
 
-  test('Should call save secure with correct values', () async {
-    await sut.saveSecure(key: key, value: value);
+  group('saveSecure', () {
+    test('Should call save secure with correct values', () async {
+      await sut.saveSecure(key: key!, value: value);
 
-    verify(() => secureStorage.write(key: key, value: value));
-  });
+      verify(() => secureStorage.write(key: key!, value: value));
+    });
 
-  test('Should throw if save secure throws', () async {
-    secureStorage.mockSaveError();
-    final future = sut.saveSecure(key: key, value: value);
+    test('Should throw if save secure throws', () async {
+      secureStorage.mockSaveError();
+      final future = sut.saveSecure(key: key!, value: value);
 
-    expect(future, throwsA(const TypeMatcher<Exception>()));
-  });
+      expect(future, throwsA(const TypeMatcher<Exception>()));
+    });
+  }); 
+
+  group('fetchSecure', () {
+    test('Should call fetch secure with correct values', () async {
+      await sut.fetchSecure(key!);
+
+      verify(() => secureStorage.read(key: key!));
+    });
+  }); 
 }
