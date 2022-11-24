a156add684610cf53443e11720568fa5466ee332
cadumeneses <carloseduardoms646@gmail.com>
2022-11-24 10:42:37 -0300

    test: ensure RemoteAddAcoount calls HttpCLient with correct values

---
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -0,0 +1,43 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_tdd/domain/usecases/usecase.dart';
+
+import 'package:flutter_tdd/data/http/http.dart';
+import 'package:flutter_tdd/data/usecases/usecases.dart';
+
+class HttpClientSpy extends Mock implements HttpClient {}
+
+void main() {
+  late RemoteAddAccount sut;
+  late HttpClientSpy httpClient;
+  late String url;
+  late AddAccountParams params;
+
+  setUp(() {
+    httpClient = HttpClientSpy();
+    url = faker.internet.httpUrl();
+    sut = RemoteAddAccount(httpClient: httpClient, url: url);
+    params = AddAccountParams(
+      name: faker.person.name(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      passwordConfirmation: faker.internet.password(),
+    );
+  });
+  test('Should call HttpClient with correct values', () async {
+    await sut.add(params);
+
+    verify(httpClient.request(
+      url: url,
+      method: 'post',
+      body: {
+        'name': params.name,
+        'email': params.email,
+        'password': params.password,
+        'passwordConfirmation': params.passwordConfirmation,
+      },
+    ));
+  });
+}
