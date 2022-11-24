cf2451aa78978551b23d486520affdf9bd2da297
cadumeneses <carloseduardoms646@gmail.com>
2022-11-24 11:10:16 -0300

    test: ensure RemoteAddAccount throws UnexpectedError throws 400

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -1,5 +1,4 @@
 import 'package:faker/faker.dart';
-import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_tdd/domain/usecases/usecase.dart';
@@ -7,6 +6,7 @@ import 'package:flutter_tdd/domain/helpers/helpers.dart';
 
 import 'package:flutter_tdd/data/http/http.dart';
 import 'package:flutter_tdd/data/usecases/usecases.dart';
+import 'package:mocktail/mocktail.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
@@ -15,6 +15,13 @@ void main() {
   late HttpClientSpy httpClient;
   late String url;
   late AddAccountParams params;
+
+  When mockRequest() =>
+    when(()=>httpClient.request(url: any(named:'url'), method: any(named:'method'), body: any(named:'body')));
+
+  void mockHttpError(HttpError error) {
+    mockRequest().thenThrow(error);
+  }
 
   setUp(() {
     httpClient = HttpClientSpy();
@@ -30,7 +37,7 @@ void main() {
   test('Should call HttpClient with correct values', () async {
     await sut.add(params);
 
-    verify(httpClient.request(
+    verify(()=>httpClient.request(
       url: url,
       method: 'post',
       body: {
@@ -42,13 +49,16 @@ void main() {
     ));
   });
 
-  test('Should throw UnexpectedError if httpClient returns 404', () async {
+  test('Should throw UnexpectedError if httpClient returns 400', () async {
+    mockHttpError(HttpError.badRequest);
 
-    when(httpClient.request(
-            url: anyNamed('url'),
-            method: anyNamed('method'),
-            body: anyNamed('body')))
-        .thenThrow(HttpError.notFound);
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if httpClient returns 404', () async {
+    mockHttpError(HttpError.notFound);
 
     final future = sut.add(params);
 
